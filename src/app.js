1361d0906fd79e4237ae0171e25d4805cd4dc5d3
Tanmay Gupta <tanmay14605@gmail.com>
2025-08-25 09:49:42 +0530

    API response

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express()
+
+export default app 
+
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import app from "./app.js"
+dotenv.config({
+    path:"./.env"
+})//this is helpful when we will be shifting the main entering file to some new folder later on in the project
+
+/*let myusername = process.env.MY_USERNAME
+console.log(myusername)*/
+
+
+
+const port=process.env.PORT || 3000
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
+    
